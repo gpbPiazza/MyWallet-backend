3f12df7d2b475f43494c10e991315d0398c2f635
Gabriel Pedro B Piazza <pedro.piazza99@gmail.com>
2021-01-25 22:54:26 -0300

    change schme update cash

--- src/schemas/accountSchemas.ts
+++ src/schemas/accountSchemas.ts
@@ -2,6 +2,6 @@ import * as Joi from 'joi'
 
 export const attBalanceSchema = Joi.object({
   value: Joi.string().required(),
-  description: Joi.string().min(2).max(30).required(),
+  description: Joi.string().min(1).max(30).required(),
   typeTransaction: Joi.string().min(7).max(10).required()
 })
